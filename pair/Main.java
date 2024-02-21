a5c26327954ef2c84e552817a077f7d00f222113
Ramil Hagverdiyev <ramilhagverdiyev@Ramils-MacBook-Pro.local>
2024-02-21 19:45:42 +0400

    override toString for RecordPair

--- pair/Main.java
+++ pair/Main.java
@@ -3,13 +3,13 @@ package pair;
 public class Main {
     public static void main(String[] args) {
         // use our Class Pair
-        Pair<String, Integer> pair = new Pair<>("aaaa", 132423);
+        Pair<String, Integer> pair = new Pair<>("aaaa", 1234);
         System.out.println(pair.getFirst());
         System.out.println(pair.getSecond());
         System.out.println(pair.toString());
 
         // use record instead of Class
-        RecordPair<String, Integer> recordPair = new RecordPair<>("aaaa", 1232423);
+        RecordPair<String, Integer> recordPair = new RecordPair<>("aaaa", 1234);
         System.out.println(recordPair.a());
         System.out.println(recordPair.b());
         System.out.println(recordPair.toString());
--- pair/RecordPair.java
+++ pair/RecordPair.java
@@ -1,4 +1,8 @@
 package pair;
 
 public record RecordPair<A, B> (A a, B b) {
+    @Override
+    public String toString() {
+        return "[Pair \na= " + a + "\nb= " + b + "]";
+    }
 }
